f42a5aa80a7bee2020332ae6662ed99c2ae5bda3
aishubalkitta <113792644+aishubalkitta@users.noreply.github.com>
2022-11-23 20:48:45 +0530

    Create StudentRepository.java

---
+++ StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demowork.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demowork.model.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+}
